592ecbff636730b9abc9e7104a4b4380bc819177
Tom Dyer <tdyer1@gmail.com>
2015-05-21 23:13:16 -0400

    Added movie show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,21 @@
 class MoviesController < ApplicationController
   # TEMPORARY ONLY!!
-  def movies 
+  def movies
     [
       {name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123},
       {name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154},
       {name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105}
     ]
-  end 
+  end
 
   # GET /movies
   def index
     render :json => movies.to_json
+  end
+
+  # GET /movies/:id
+  def show
+    id = params[:id].to_i
+    render :json => movies[id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'movies', to: 'movies#index'
+  get 'movies/:id', to: 'movies#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
