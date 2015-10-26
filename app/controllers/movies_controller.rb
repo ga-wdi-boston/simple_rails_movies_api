37ee5fd387750cd086a39a8431ab30c952506f9a
Tom Dyer <tdyer1@gmail.com>
2015-10-26 09:03:50 -0400

    Started create action, prob need ActiveRecord for this!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,21 +1,34 @@
 class MoviesController < ApplicationController
   # TEMPORARY ONLY!!
-  def movies
-    [
+  @@movies = [
       {name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123},
       {name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154},
       {name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105}
     ]
-  end
+
 
   # GET /movies
   def index
-    render :json => movies.to_json
+    render :json => @@movies.to_json
   end
 
   # GET /movies/:id
   def show
     id = params[:id].to_i
-    render :json => movies[id]
+    render :json => @@movies[id]
+  end
+
+  # POST /movies
+  def create
+    @movie = movie_params['movie']
+    @@movies << @movie
+    # logger.debug("Movies are " + @@movies.inspect);
+    render :json => @movie.to_json
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :rating, :desc, :length)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'movies', to: 'movies#index'
   get 'movies/:id', to: 'movies#show'
+  post 'movies', to: 'movies#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
