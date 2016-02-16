8b143f554c113816726858cc673db6069215c910
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-15 22:57:06 -0500

    Add `id` as a property of the movie
    This makes the example closer to how Rails actually works.
    
    Additionally, the `movies` helper method gets made private.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,4 @@
 class MoviesController < ApplicationController
-  # TEMPORARY ONLY!!
-  def movies
-    [
-      {name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123},
-      {name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154},
-      {name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105}
-    ]
-  end
-
   # GET /movies
   def index
     render :json => movies.to_json
@@ -16,6 +7,16 @@ class MoviesController < ApplicationController
   # GET /movies/:id
   def show
     id = params[:id].to_i
-    render :json => movies[id]
+    render :json => movies.find {|movie| movie[:id] == id}
+  end
+
+  private
+  # TEMPORARY - ONLY FOR DEMONSTRATION!
+  def movies
+    [
+      {id: 3, name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123},
+      {id: 7, name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154},
+      {id: 10, name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105}
+    ]
   end
 end
